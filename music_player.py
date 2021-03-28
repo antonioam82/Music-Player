5b833b2908cd7fea23ef549c9be21981c9e60c68
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2021-03-28 15:13:54 +0200

    Update music_player.py

--- music_player.py
+++ music_player.py
@@ -2,15 +2,21 @@ from tkinter import *
 import tkinter.scrolledtext as sct
 from tkinter import filedialog, messagebox
 import playsound
+import os
 
 class Player:
     def __init__(self):
         self.root = Tk()
         self.root.title("Music Player")
-        self.root.geometry("800x290")
+        self.root.geometry("803x301")
 
-        self.timer = Label(self.root,text="0:00:00",bg="black",fg="green",font=("arial","29"),width=10,height=2)
-        self.timer.place(x=15,y=20)
+        self.currentDir = StringVar()
+        self.currentDir.set(os.getcwd())
+
+        entryDir = Entry(self.root,textvariable=self.currentDir,width=133)
+        entryDir.place(x=0,y=0)
+        self.timer = Label(self.root,text="0:00:00",bg="black",fg="green",font=("arial","33"),width=14,height=2)
+        self.timer.place(x=9,y=28)
 
         self.root.mainloop()
 
