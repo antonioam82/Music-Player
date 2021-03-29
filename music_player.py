c17b28fc9a2c5f007b5bf000d9541478f7c2320e
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2021-03-29 16:39:03 +0200

    Update music_player.py

--- music_player.py
+++ music_player.py
@@ -10,13 +10,17 @@ class Player:
         self.root.title("Music Player")
         self.root.geometry("803x301")
 
+        self.filename = StringVar()
         self.currentDir = StringVar()
         self.currentDir.set(os.getcwd())
 
         entryDir = Entry(self.root,textvariable=self.currentDir,width=133)
         entryDir.place(x=0,y=0)
-        self.timer = Label(self.root,text="0:00:00",bg="black",fg="green",font=("arial","33"),width=14,height=2)
+        self.timer = Label(self.root,text="0:00:00",bg="black",fg="green",font=("arial","34"),width=13,height=2)
         self.timer.place(x=9,y=28)
+        self.entryFile = Entry(self.root,textvariable=self.filename,width=29,font=("arial",20))
+        self.entryFile.place(x=355,y=28)
+        Button(self.root,text="SEARCH",width=61,bg="light gray").place(x=356,y=65)
 
         self.root.mainloop()
 
