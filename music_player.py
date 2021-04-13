bc5cc2f3eb7952730db5e8aa62d2349ac48c50fb
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2021-04-13 17:45:54 +0200

    Update music_player.py

--- music_player.py
+++ music_player.py
@@ -140,20 +140,23 @@ been deleted or moved.''')
         self.process=self.timer.after(1000,self.timer_count)
 
     def music(self):
-        self.playing = True
-        self.p = pyaudio.PyAudio()
-        wf = wave.open(self.file_path, 'rb')
-        self.stream = self.p.open(format=self.p.get_format_from_width(wf.getsampwidth()),
-                    channels=wf.getnchannels(),rate=wf.getframerate(),output=True)
-        data = wf.readframes(self.CHUNK)
-        self.timer_count()###############################3
-        while data and self.playing == True:
-            self.stream.write(data)
+        try:
+            self.playing = True
+            self.p = pyaudio.PyAudio()
+            wf = wave.open(self.file_path, 'rb')
+            self.stream = self.p.open(format=self.p.get_format_from_width(wf.getsampwidth()),
+                        channels=wf.getnchannels(),rate=wf.getframerate(),output=True)
             data = wf.readframes(self.CHUNK)
-        self.timer.after_cancel(self.process)###################################
-        self.stream.stop_stream()
-        self.stream.close()
-        self.p.terminate()
+            self.timer_count()###############################3
+            while data and self.playing == True:
+                self.stream.write(data)
+                data = wf.readframes(self.CHUNK)
+            self.timer.after_cancel(self.process)###################################
+            self.stream.stop_stream()
+            self.stream.close()
+            self.p.terminate()
+        except Exception as e:
+            messagebox.showwarning("UNEXPECTED ERROR",str(e))
         self.playing = False
 
     def get_key(self,val):
