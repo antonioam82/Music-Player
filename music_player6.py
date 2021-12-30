358dc39f95e86d118924f37de0d10c5481084ed2
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2021-12-30 10:51:48 +0100

    Update music_player6.py

--- music_player6.py
+++ music_player6.py
@@ -4,9 +4,7 @@ from tkinter import *
 from tkinter import filedialog, messagebox
 import random
 import time
-#import mutagen
-import pygame#############################################
-from pygame import mixer, display#####################################
+from pygame import mixer, display
 import threading
 import json
 import time
