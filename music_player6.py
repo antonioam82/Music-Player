6d21d28fc31b68607d591d7c25d8862de1e6e155
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2022-02-07 19:29:38 +0100

    Update music_player6.py

--- music_player6.py
+++ music_player6.py
@@ -83,7 +83,8 @@ class Player:
             self.filename.set(self.file_path.split("/")[-1])
             
     def add(self):
-        if self.entryFile.get() != "" and self.running == False: 
+        self.any_selected = self.is_any_selected()
+        if self.entryFile.get() != "" and self.running == False and self.any_selected == False:
             self.fav_list.delete(0,END)
             self.audio_list[self.filename.get()]=self.file_path
             with open("music_favs.json", "w") as f:
