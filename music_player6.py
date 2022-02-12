020b43f28df1eaada920327a68a9b6a8455d10ee
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2022-02-12 19:37:47 +0100

    Update music_player6.py

--- music_player6.py
+++ music_player6.py
@@ -236,7 +236,6 @@ class Player:
                             else:
                                 listado = self.create_list(self.my_list,listado[c])
                                 c = 0
-                        #pass
             else:
                 if c != 0:
                     c = 0
